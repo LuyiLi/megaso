dd434d3041fa3ff76e8f9174b2e7255f7b5901e9
Zhaohan Wang <wqwangzhaohan@126.com>
2019-03-04 23:54:14 +0800

    Player类完善、架构优化
    Player类完善、架构优化

--- inc/Player.h
+++ inc/Player.h
@@ -1,10 +1,12 @@
 #pragma once
+#include "LTexture.h"
 //Åö×²µãÀà
 extern int pos_x;
 extern int pos_y;
 extern const int g;
 extern const int SCREEN_WIDTH;
 extern const int SCREEN_HEIGHT;
+
 class Player
 {
 public:
@@ -24,13 +26,27 @@ public:
 	//Moves the Player and checks collision
 	void move(SDL_Rect& wall);
 
+	void moveAction();
+	bool initPlayerTexture();
+
 	//Shows the Player on the screen
-	void render();
+	void render(int camX, int camY);
+
+	int getPosX();
+	int getPosY();
 
 	//The velocity of the Player
 	int mVelX, mVelY;
 
 	//Player's collision box
 	SDL_Rect mCollider;
-};
 
+	/*Õ¾Á¢×´Ì¬Í¼ÏñÇÐÆ¬*/
+	//const int standing_frames = 6;
+	SDL_Rect slime_stand_clips[6];
+	LTexture slime_standing_texture;
+	/*ÐÐ×ß×´Ì¬Í¼ÏñÇÐÆ¬*/
+	//const int walking_frames = 4;
+	SDL_Rect slime_walk_clips[4];
+	LTexture slime_walking_texture;
+};
--- src/LTexture.cpp
+++ src/LTexture.cpp
@@ -60,6 +60,43 @@ bool LTexture::loadFromFile(std::string path)
 	return mTexture != NULL;
 }
 
+#ifdef _SDL_TTF_H
+bool LTexture::loadFromRenderedText(std::string textureText, SDL_Color textColor)
+{
+	//Get rid of preexisting texture
+	free();
+
+	//Render text surface
+	SDL_Surface* textSurface = TTF_RenderText_Solid(gFont, textureText.c_str(), textColor);
+	if (textSurface != NULL)
+	{
+		//Create texture from surface pixels
+		mTexture = SDL_CreateTextureFromSurface(gRenderer, textSurface);
+		if (mTexture == NULL)
+		{
+			printf("Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError());
+		}
+		else
+		{
+			//Get image dimensions
+			mWidth = textSurface->w;
+			mHeight = textSurface->h;
+		}
+
+		//Get rid of old surface
+		SDL_FreeSurface(textSurface);
+	}
+	else
+	{
+		printf("Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError());
+	}
+
+
+	//Return success
+	return mTexture != NULL;
+}
+#endif
+
 void LTexture::free()
 {
 	//Free texture if it exists
